e319df55c95454a71fb712da2dc8f4ebc2f3c597
svetlanajJd <artlanago@gmail.com>
2023-04-07 17:38:26 +0300

    first

---
+++ src/main/java/pro/sky/java/sockswarehouse/SocksWarehouseApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.sockswarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocksWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocksWarehouseApplication.class, args);
+	}
+
+}
